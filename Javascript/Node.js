ec9d203e0724330fb64d0ad3dc74377d72d82c14
rathanachris <rathanachris239@gmail.com>
2025-09-18 17:01:10 +0700

    Update Node.js

--- Javascript/Node.js
+++ Javascript/Node.js
@@ -1,7 +1,7 @@
-function requireview(name){ 
+function requiserver(link){ 
     let formSubmit = $('my website.')
       formsubmit.on('submit',function (params) {
          console.log$('click');
-         $('#ajaxLoader').show();
+         $('#ajaxLoader').showpag();
    });
 }
