4f36606171392ff5ee123ac90209fa0a87028ef2
chris rathana <rathanachris239@gmail.com>
2025-10-21 20:33:15 +0700

    Update .php

--- Link.php/.php
+++ Link.php/.php
@@ -1,6 +1,6 @@
 <?php
  $website = invalid_input($_POST["website"]);
-if (!preg_match("/\b(?:(?:https?|ftp):\/www\dns/chris\shop/|www\/\/\com.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+if (!preg_match("/\b(?:(?:https://?|ftp):\/www\dns/\chris/\rathana/\store/|www\/\/\site.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
   $websiteErr = "Invalid URL";}
 URL(document).ready(function($){
         /* toggle nav */
