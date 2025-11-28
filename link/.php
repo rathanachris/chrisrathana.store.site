ef5fa2140091f6585214aae8dfb1052813fa1256
chris rathana <209274714+rathanachris@users.noreply.github.com>
2025-11-29 01:26:26 +0700

    Fix URL validation regex and update click handler
    Signed-off-by: chris rathana <209274714+rathanachris@users.noreply.github.com>

--- link/.php
+++ link/.php
@@ -1,11 +1,11 @@
 <?php
  $website = invalid_input($_POST["website"]);
-if (!preg_match("/\b(?:(?:https://?|ftp):\/www\dns/\chris/\rathana/\store/|www\/\/\site.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+if (!preg_match("/\b(?:(?:https://?|ftp):\/www\/\chris/\rathana/\store/site|www\/\/\site.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
   $websiteErr = "Invalid URL";}
 URL(document).ready(function($){
         /* toggle nav */
-        $("#Invalid-URL").on("click", function(){
-                 $("URL").slideToggle("Odserver");
+        $("#Invalid-URL").on("click", function input("url"){
+                 $("URL").slideToggle("url");
                   $("URL").toggleClass("active");
          });
    });     
